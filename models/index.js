b5a4ddcbba3d5ceff4b17f123a8d8efa16c4d969
Rico <rikardoan@mail.com>
2021-12-30 00:31:14 -0500

    catebory included products

--- models/index.js
+++ models/index.js
@@ -5,11 +5,50 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+// Product.init(
+//   {
+ 
 
-Product.belongsTo(Category);
+  //   id: {
+  //     type: DataTypes.INTEGER,
+  //     allowNull: false,
+  //     primaryKey: true,
+  //     autoIncrement: true,
+  //   },
+  //   product_name: {
+  //     type: DataTypes.STRING,
+  //     allowNull: false,
+  //   },
+  //   price: {
+  //     type: DataTypes.INTEGER,
+  //     allowNull: false,
+  //   },
+  //   stock: {
+  //     type: DataTypes.INTEGER,
+  //     allowNull: false,
+  //   },
+  //   category_id: {
+  //     type: DataTypes.INTEGER,
+  //     references: {
+  //       model: "category",
+  //       key: "id",
+  //     },
+  //   },
+  // },
+
+Product.belongsTo(Category,
+//   {
+//   foreignKey:'product_id',
+//   onDelete: 'CASCADE',
+// }
+
+  );
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey:'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,9 @@ router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products\\
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll(
+      { include:[{model: Product}],}
+    );
     res.status(200).json(categoryData);
   } catch (error) {
     res.status(400).json(err);
@@ -18,15 +20,63 @@ router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 
-  Category.findByPk(req.params.id).then((bookData) => {
-    res.json(bookData);
-    console.log(bookData)
+  try {
+    const categoryData = await Category.findByPk(req.params.id,{
 
-  });
+      include:[{model: Product}],
+    });
+    // console.log("include product-model"+categoryData)
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(err);
+    
+  } // Category.findByPk(req.params.id).then((bookData) => {
+  //   res.json(bookData);
+  //   console.log(bookData)
+
+  // });
+
+
+  
 });
 
 router.post("/", (req, res) => {
   // create a new category
+//--
+
+
+  /* req.body should look like this...
+    {
+      category_name: "skate",
+      
+    }
+  */
+  Product.create(req.body)
+    .then((category) => {
+      // if there's category tags, we need to create pairings to bulk create in the categoryTag model
+      // if (req.body.tagIds.length) {
+      //   const categoryTagIdArr = req.body.tagIds.map((tag_id) => {
+      //     return {
+      //       category_id: category.id,
+      //       tag_id,
+      //     };
+      //   });
+      //   return categoryTag.bulkCreate(categoryTagIdArr);
+      // }
+      // if no category tags, just respond
+      res.status(200).json(category);
+    })
+    .then((categoryTagIds) => res.status(200).json(categoryTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+
+
+
+
+
+  //--
 });
 
 router.put("/:id", (req, res) => {
